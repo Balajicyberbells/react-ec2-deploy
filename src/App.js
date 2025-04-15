62d5aad1c393bf16b56916e808d097d65c230830
balaji <balajivetcha.cyberbells@gmail.com>
2025-04-15 12:45:51 +0530

    react app files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Cyber bells!</h1>
+      <p>This is a simple ReactJS app.</p>
+    </div>
+  );
+}
+
+export default App;
