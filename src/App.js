b058351d5f4be5bf799ac43b0605059858194dd3
balajivetcha <balajivetcha.cyberbells@gmail.com>
2025-04-25 00:24:37 +0530

    added some text

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Welcome to Cyber bells!</h1>
       <p>This is a simple ReactJS app.</p>
+	<h1> Your app is ready </h1>	  
     </div>
   );
 }
